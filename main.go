5fd51e133d42ed4f190489fb9c7bff990b4b38f3
xiaoboya <xiaoboya@owntek.com>
2020-06-20 10:24:52 +0800

    add:更改文件结构

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 /*
  * @Author: xiaoboya
  * @Date: 2020-06-19 16:03:52
- * @LastEditTime: 2020-06-19 16:04:10
+ * @LastEditTime: 2020-06-20 10:24:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /MyDiskClient/main.go
  */
 
 package main
+
+func main() {}
---
+++ utils/connectPlatform.go
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-20 10:24:29
+ * @LastEditTime: 2020-06-20 10:24:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /MyDiskClient/utils/connectPlatform.go
+ */
+
+package utils
