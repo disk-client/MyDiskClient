5e36535780aeab385ab6046607213dafb5e653a2
xiaoboya <xiaoboya@owntek.com>
2020-06-20 12:55:45 +0800

    add:添加登录代码

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: xiaoboya
  * @Date: 2020-06-19 16:03:52
- * @LastEditTime: 2020-06-20 11:39:09
+ * @LastEditTime: 2020-06-20 12:44:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /MyDiskClient/main.go
@@ -9,8 +9,17 @@
 
 package main
 
-import "MyDiskClient/utils"
+import (
+	"MyDiskClient/utils"
+	"fmt"
+	"net"
+)
+
+// SRCADDR UDP服务路径
+var SRCADDR = &net.UDPAddr{IP: net.IPv4zero, Port: 7611}
 
 func main() {
 	utils.Init()
+	var msg = utils.Login("xiaoboya", "root1234")
+	fmt.Println(msg)
 }
--- utils/connectPlatform.go
+++ utils/connectPlatform.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-20 10:24:29
- * @LastEditTime: 2020-06-20 11:43:58
+ * @LastEditTime: 2020-06-20 12:54:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /MyDiskClient/utils/connectPlatform.go
@@ -24,12 +24,21 @@ func Login(username, cred string) (msg string) {
 		"password": cred,
 	}
 	var jsonStr, _ = json.Marshal(params)
+	fmt.Println(string(jsonStr))
 	var client = &http.Client{}
 	var loginURL = fmt.Sprintf("http://%s:8080/login", Host)
-	resp, err := client.Post("POST", loginURL, bytes.NewBuffer(jsonStr))
+	resp, err := client.Post(loginURL, "application/json", bytes.NewBuffer(jsonStr))
 	if err != nil {
+		fmt.Println(err)
 		return "连接管理平台失败"
 	}
 	result, _ := ioutil.ReadAll(resp.Body)
-	return string(result)
+	var data map[string]map[string]interface{}
+	err = json.Unmarshal(result, &data)
+	if err != nil {
+		return err.Error()
+	}
+	var content = data["data"]["info"]
+	msg, _ = content.(string)
+	return msg
 }
